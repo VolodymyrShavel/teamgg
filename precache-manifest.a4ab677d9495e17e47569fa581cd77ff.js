1d75e0db75db8c0b73a67f9b0aab22ec23e921f6
Volodia <volodiasyd@gmail.com>
2021-12-12 16:37:56 +0200

    Updates

---
+++ precache-manifest.a4ab677d9495e17e47569fa581cd77ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b521e2faf0cd4c8ccab58ac3887944f",
+    "url": "/teamgg/index.html"
+  },
+  {
+    "revision": "3857b5feb82609b19011",
+    "url": "/teamgg/static/css/main.43a0810e.chunk.css"
+  },
+  {
+    "revision": "33914cdbda493d81965a",
+    "url": "/teamgg/static/js/2.f8d3a973.chunk.js"
+  },
+  {
+    "revision": "540ab96cbe906c6bca4bbe6a86d0a45d",
+    "url": "/teamgg/static/js/2.f8d3a973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3857b5feb82609b19011",
+    "url": "/teamgg/static/js/main.1af2181c.chunk.js"
+  },
+  {
+    "revision": "5125b266117f6a9945c4",
+    "url": "/teamgg/static/js/runtime-main.3c97f490.js"
+  }
+]);
